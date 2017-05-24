ad75eae2f6965c17b262346968acd87364152426
Will <willzhang100@gmail.com>
2017-05-24 19:43:30 -0400

    Simple life renderer.

--- life.py
+++ life.py
@@ -1,14 +1,60 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from tkinter import *
+import time
+import copy
 
-def setBoard(board, initial_state):
+class Life:
+
+    def __init__(self, path):
+        self.width, self.height = 400, 400
+        self.col, self.row = 10, 10
+        self.w = 40
+        self.p = 5
+        self.board = self.build(path)
+        self.c = self.setup()
+        self.c.pack()
+
+        self.render()
+        mainloop()
+
+    def setup(self):
+        master = Tk()
+        return Canvas(master, width=405, height=405)
+
+    def build(self, path):
+        board = []
+        with open(path) as f:
+            data = f.read()
+            for line in data.splitlines():
+                cleaned = line.replace(" ", "") # removes all whitespace
+                board.append(list(cleaned))
+        return board
+
+    def render(self):
+        for i in range(self.row):
+            for j in range(self.col):
+                if self.board[i][j] == "1": # the cell is alive
+                    self.c.create_rectangle(j*self.w+self.p, i*self.w+self.p, (j+1)*self.w+self.p, (i+1)*self.w+self.p, fill="black")
+                else:
+                    self.c.create_rectangle(j*self.w+self.p, i*self.w+self.p, (j+1)*self.w+self.p, (i+1)*self.w+self.p, fill="white")
+        self.c.after(500, self.generate) # wait 500 ms then generate the new board
+
+    def generate(self):
+        nextboard = copy.deepcopy(self.board)
+        # only consider the non-border cells
+        for i in range(1,self.row-1):
+            for j in range(1,self.col-1):
+                status = self.board[i][j]
+                neighbors = [self.board[i-1][j-1], self.board[i-1][j], self.board[i-1][j+1], self.board[i][j-1], self.board[i][j+1], self.board[i+1][j-1], self.board[i+1][j], self.board[i+1][j+1]]
+                alive = neighbors.count("1")
+                if status == "1" and alive < 2: # death by underpopulation
+                    nextboard[i][j] = "0"
+                elif status == "1" and alive > 3: # death by overpopulation
+                    nextboard[i][j] = "0"
+                elif status == "0" and alive == 3: # birth by reproduction
+                    nextboard[i][j] = "1"
+        self.board = nextboard
+        self.render()
 
 if __name__ == '__main__':
-    grid = 100
-    board = np.zeros((grid,grid))
-    for yi in xrange(grid):
-        for xi in xrange(grid):
-            if (xi + yi) % 2 == 0:
-                board[yi][xi] = 1
-    plt.pcolormesh(board)
-    plt.show()
+    path = "board_1.txt"
+    life = Life(path)
